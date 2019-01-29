1bf1fefc4b6be110a75ec8cc5c32cd14915423cb
Kenneth Berry <rosskberry@gmail.com>
2019-01-29 12:04:01 -0700

    Update SumsToN algorithm

--- SumsToN.java
+++ SumsToN.java
@@ -1,40 +1,47 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
-public class SumsToN {
-    
-    private static int n;
-    private static ArrayList<ArrayList<Integer>> sums;
+class SumsToN {
 
-    public static ArrayList<ArrayList<Integer>> sumsOfN(int n){
-        ArrayList<ArrayList<Integer>> sumsList = new ArrayList<ArrayList<Integer>>();
-        sumsList.add(new ArrayList<Integer>(n));
-        for (int i = 1, j = n - 1; i < (n/2) + 1; i++, j--){
-            ArrayList<Integer> tempList = new ArrayList<Integer>();
-            tempList.add(i);
-            tempList.add(j);
-            sumsList.add(tempList);
+private static int[] sums;
+
+    static void findSums(int[] arr, int index, int n, int reducedNum) {
+        if (reducedNum < 0) {
+            return;
         }
-        return sumsList;
+
+        if (reducedNum == 0) {
+            for (int i = 0; i < index; i++) {
+                if (i + 1 == index) {
+                    System.out.print (arr[i]);
+                }
+                else {
+                    System.out.print (arr[i] + " + ");
+                }
+            }
+            System.out.println();
+            return;
+        }
+
+        int prev;
+        if (index == 0) {
+            prev = 1;
+        }
+        else {
+            prev = arr[index - 1];
+        }
+
+        for (int i = prev; i <= n ; i++) {
+            arr[index] = i; 
+            findSums(arr, index + 1, n, reducedNum - i);
+        }
     }
 
-    public static void main(String[] args) {
-
+    public static void main (String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter a positive integer:");
         int n = reader.nextInt();
         reader.close();
-        sums = sumsOfN(n);
-        for (int i = 0; i < sums.size(); i ++){
-            for (int j = 0; j < sums.get(i).size(); j ++){
-                System.out.print(sums.get(i).get(j));
-                if (sums.get(i).get(j) != sums.get(i).size()){
-                    System.out.print(" + ");
-                }
-            }
-            System.out.println();
-        }
-        
+        sums = new int[n]; 
+        findSums(sums, 0, n, n); 
     }
-
 }
