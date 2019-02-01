b194b637e3ec92ecd22a305b6235eae2a46b3948
Kenneth Berry <kberry14@msudenver.edu>
2019-01-31 18:59:50 -0700

    Add debug output

--- SumsToN/SumsToN.java
+++ SumsToN/SumsToN.java
@@ -7,23 +7,26 @@
  * @author Kenneth Berry
  */
 
-
 import java.util.Scanner;
 
 class SumsToN {
 
-    public static void printSumsToN(int[] arr, int index, int n, int current) {
-        if (current < 0) {
+    public static void printSumsToN(int arr[], int index, int countdown, int n) {
+        if (countdown < 0) {
             return;
         }
 
-        if (current == 0) {
+        if (countdown == 0) {
+            // DEBUG - print contents of array
+            // for (int i = 0; i < n; i++){
+            // System.out.print(arr[i]);
+            // }
+            // System.out.println();
             for (int i = 0; i < index; i++) {
                 if (i + 1 == index) {
-                    System.out.print (arr[i]);
-                }
-                else {
-                    System.out.print (arr[i] + " + ");
+                    System.out.print(arr[i]);
+                } else {
+                    System.out.print(arr[i] + " + ");
                 }
             }
             System.out.println();
@@ -33,23 +36,22 @@ class SumsToN {
         int prev;
         if (index == 0) {
             prev = 1;
-        }
-        else {
+        } else {
             prev = arr[index - 1];
         }
 
-        for (int i = prev; i <= n ; i++) {
-            arr[index] = i; 
-            printSumsToN(arr, index + 1, n, current - i);
+        for (int x = prev; x <= n; x++) {
+            arr[index] = x;
+            printSumsToN(arr, index + 1, countdown - x, n);
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter a positive integer:");
         int n = reader.nextInt();
         reader.close();
-        int sums[] = new int[n]; 
-        printSumsToN(sums, 0, n, n); 
+        int arr[] = new int[n];
+        printSumsToN(arr, 0, n, n);
     }
 }
