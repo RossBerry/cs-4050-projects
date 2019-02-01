8278cdd2da8eb82a7e6f067fe2851928e107a9b8
Kenneth Berry <kberry14@msudenver.edu>
2019-01-31 19:01:02 -0700

    Update to conform to PEP8

--- SumsToN/SumsToN.py
+++ SumsToN/SumsToN.py
@@ -7,10 +7,10 @@ by the user in the console.
 """
 __author__ = "Kenneth Berry"
 
-def sums_to_n(sums_lst, index, input_n, current):
-    """ 
+def sums_to_n(num_list, index, input_n, current):
+    """
     Recursively finds all sums adding up to n and prints
-    them to the console 
+    them to the console
     """
     if current < 0:
         return
@@ -19,9 +19,9 @@ def sums_to_n(sums_lst, index, input_n, current):
         out_string = ""
         for i in range(index):
             if (i + 1) == index:
-                out_string += str(sums_lst[i])
+                out_string += str(num_list[i])
             else:
-                out_string += (str(sums_lst[i]) + " + ")
+                out_string += (str(num_list[i]) + " + ")
 
         print(out_string)
         return
@@ -29,15 +29,15 @@ def sums_to_n(sums_lst, index, input_n, current):
     if index == 0:
         prev = 1
     else:
-        prev = sums_lst[index - 1]
+        prev = num_list[index - 1]
 
-    while(prev <= input_n):
-        sums_lst[index] = prev;
-        sums_to_n(sums_lst, index + 1, input_n, current - prev)
+    while prev <= input_n:
+        num_list[index] = prev
+        sums_to_n(num_list, index + 1, input_n, current - prev)
         prev += 1
 
 
 if __name__ == "__main__":
-    n = int(input("Enter a positive integer: "))
-    zero_lst = [0 for num in range(n)]
-    sums_to_n(zero_lst, 0, n, n)
+    INPUT_N = int(input("Enter a positive integer: "))
+    ZERO_LIST = [0 for num in range(INPUT_N)]
+    sums_to_n(ZERO_LIST, 0, INPUT_N, INPUT_N)
